33cedf5674d8772740212b585203be2fdf256d53
SohnSangbum <foot0707@gmail.com>
2025-09-06 21:54:43 +0900

    상범 추가 250906 PM 21:54

---
+++ src/component/streaming/latestmusic/LatestMusic.jsx
@@ -0,0 +1,7 @@
+import './style.scss';
+
+const LatestMusic = () => {
+    return <section></section>;
+};
+
+export default LatestMusic;
---
+++ src/page/streaming/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+const index = () => {
+    return <div id="streaming home"></div>;
+};
+
+export default index;
