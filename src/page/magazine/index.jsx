aff55867235404c899b4307275e2deee075a578f
SongTam-tam <gacbam0315@gmail.com>
2025-09-10 17:00:36 +0900

    123

--- src/page/magazine/index.jsx
+++ src/page/magazine/index.jsx
@@ -1,11 +1,7 @@
-import Magazine1 from '../../component/Magazine/magazine1/Magazine1';
+// import Magazine1 from '../../component/Magazine/magazine1/Magazine1';
 
 const Magazine = () => {
-    return (
-        <div>
-            <Magazine1 />
-        </div>
-    );
+    return <div>{/* <Magazine1 /> */}</div>;
 };
 
 export default Magazine;
