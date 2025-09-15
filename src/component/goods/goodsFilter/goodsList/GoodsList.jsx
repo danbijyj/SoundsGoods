5791d584b8cc43dc3dfd2e5feebdc455ad1ab27c
SongTam-tam <gacbam0315@gmail.com>
2025-09-15 09:43:39 +0900

    123

--- src/component/goods/goodsFilter/goodsList/GoodsList.jsx
+++ src/component/goods/goodsFilter/goodsList/GoodsList.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useGoodsStore } from '../../../../store';
 import GoodsItem from './GoodsItem';
 import './style.scss';
 
 const GoodsList = () => {
     const goods = useGoodsStore((state) => state.goods);
+    useEffect(() => {
+        const ids = goods.map((g) => g.id);
+        const dupes = ids.filter((id, i) => ids.indexOf(id) !== i);
+        if (dupes.length) {
+            console.error('중복 id 발견:', [...new Set(dupes)]);
+            console.table(goods.filter((g) => dupes.includes(g.id)));
+        }
+    }, [goods]);
     return (
         <ul className="goods_page_goods_list">
             {goods.map((goods) => (
